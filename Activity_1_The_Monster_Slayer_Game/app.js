dc316b0b7de81a3a4bbbfa9cdc467163485c3db0
HaizaCasapao <casapaohaiza00@gmail.com>
2021-11-05 00:58:51 -1200

    Topic 2 - Added Click Listener On html

--- Activity_1_The_Monster_Slayer_Game/app.js
+++ Activity_1_The_Monster_Slayer_Game/app.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
         attackMonster() {
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
